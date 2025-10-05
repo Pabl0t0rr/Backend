77f5badcd5431cae925e5a268e0065ee9a0212b7
Pabl0t0rr <Pablotorr04@gmail.com>
2025-10-05 17:09:17 +0200

    Creacion de ruta esclusiva + intento ejercicio .map

--- Teoria/3ºSemana/Martes/3ºSemana.ts
+++ Teoria/3ºSemana/Martes/3ºSemana.ts
@@ -2,8 +2,24 @@
 //Usaremos una libreria quese llama axios
 import axios from "axios";
 
-//Funcion que reciba nme, status,gender y segun estos parametros sacar el resultado de los personajes que coincidan con los datos dados
-// de los personajes mostrados queremos qu elos personajes so
+//Definicion de types
+type Episode = {
+    id : number;
+    name : string;
+    air_date : string;
+    episode : string;
+};
+
+type Personaje = {
+    id : number;
+    name : string;
+    status : string;
+    gender : string;
+    episode : string[];
+}
+
+//Funcion que reciba name, status,gender y según estos parametros sacar el resultado de los personajes que coincidan con los datos dados
+//De esos personajes queremos sacar los episodios en los que han salido y en los episodios en los que aparecen salgan los personajes que participaron
 
 //Funcion
 const getValues = (name : string, status : string, gender : string) => {
@@ -25,24 +41,9 @@ console.log(URLCompleta);
 //Realizar la peticion a la API con la URL Completa
     axios.get(URLCompleta)
         .then(response => {
-            console.log(response.data.results); //Mostramos los valores de la API teneindo en cuenta el filtro anterior
-            const contenidoPersonajes = response.data.results; //Guardamos en una constante los resultados de la API
-            contenidoPersonajes.map((personaje: any) => {
-                console.log(`Nombre: ${personaje.name}, Estado: ${personaje.status}, Género: ${personaje.gender}`); //Mostramos los nombre de los personajes(confirmacion)
-                const detalleEpisodios = personaje.episode.map((episodioURL: string) => {
-                        return axios.get(episodioURL)
-                            .then(episodioResponse => {
-                                console.log(episodioResponse.data); // Devolvemos los datos del episodio
-                            })
-                            .catch(error => {
-                                console.log(`Error al obtener detalles del episodio desde ${episodioURL}:`, error);
-                                return null; // En caso de error, devolvemos null
-                            })
-                            .finally(() => {
-                                console.log(`Petición finalizada para el episodio desde ${episodioURL}`);
-                            });
-                });
-            })
+            //console.log(response.data.results); //Mostramos los valores de la API teneindo en cuenta el filtro anterior
+            const contenidoPersonajes = response.data.results; //Guardamos en una constante los resultados de la API para poder buscar info a posteriori
+           
         })
         .catch(error => {
             console.log("Error al realizar la peticion de la API filtrada por nombre, status y gender"); //Mostramos mensaje error especifico
