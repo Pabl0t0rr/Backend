540ee45f52bb803cc0e2e248dad8a49df6acb178
Pabl0t0rr <Pablotorr04@gmail.com>
2025-11-04 16:29:17 +0100

    Creacion routes con distintos metodos comparativos

--- Teoria/6_7_Semana/src/routes.ts
+++ Teoria/6_7_Semana/src/routes.ts
@@ -7,7 +7,139 @@ const coleccion = () => getDb().collection("Prueba_Funcionamiento");
 
 router.get("/", async (req, res) => {
   try {
-    const personas = await coleccion().find().toArray();
+    const page = Number(req.query?.page) || 1;
+    const limit = Number(req.query?.limit) || 2;
+    const skip = (page -1) * limit;
+    const personas = await coleccion().find().skip(skip).limit(limit).toArray(); //Un cursor es un puntero de C pero en mongodb
+    res.json({
+      info :{
+        page : page,
+        numberOfPeople : limit
+      },
+      personas
+    });
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro igual 
+router.get("/", async (req, res) => {
+  try {
+    const ageQuery = Number(req.query.age);
+    const personas = await coleccion().find(
+      {age : {$eq : ageQuery} }
+    ).toArray(); //Query es todo lo que va despues del ? en url
+    res.json(personas);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro not equal 
+router.get("/", async (req, res) => {
+  try {
+    const ageQuery = Number(req.query.age);
+    const personas = await coleccion().find(
+      {age : {$ne : ageQuery} }
+    ).toArray(); //Query es todo lo que va despues del ? en url
+    res.json(personas);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro mayor a x 
+router.get("/", async (req, res) => {
+  try {
+    const ageQuery = Number(req.query.age);
+    const personas = await coleccion().find(
+      {age : {$gt : ageQuery} }
+    ).toArray(); //Query es todo lo que va despues del ? en url
+    res.json(personas);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro mayor o igual a x 
+router.get("/", async (req, res) => {
+  try {
+    const ageQuery = Number(req.query.age);
+    const personas = await coleccion().find(
+      {age : {$gte : ageQuery} }
+    ).toArray(); //Query es todo lo que va despues del ? en url
+    res.json(personas);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro menor a x 
+router.get("/", async (req, res) => {
+  try {
+    const ageQuery = Number(req.query.age);
+    const personas = await coleccion().find(
+      {age : {$lt : ageQuery} }
+    ).toArray(); //Query es todo lo que va despues del ? en url
+    res.json(personas);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro menor o igual a x 
+router.get("/", async (req, res) => {
+  try {
+    const ageQuery = Number(req.query.age);
+    const personas = await coleccion().find(
+      {age : {$lte : ageQuery} }
+    ).toArray(); //Query es todo lo que va despues del ? en url
+    res.json(personas);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro indicando cualquiera que este dentro
+router.get("/", async (req, res) => {
+  try {
+    const friendQuery = Number(req.query.friends);
+    const friendQ2uery = Number(req.query.friends1);
+    const personas = await coleccion().find(
+      {friends : {$in : [friendQuery, friendQ2uery]} }
+    ).toArray(); //Query es todo lo que va despues del ? en url
+    res.json(personas);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro indicando cualquiera que no tenga los que les pases
+router.get("/", async (req, res) => {
+  try {
+    const friendQuery = Number(req.query.friends);
+    const friendQ2uery = Number(req.query.friends1);
+    const personas = await coleccion().find(
+      {friends : {$nin : [friendQuery, friendQ2uery]} }
+    ).toArray(); //Query es todo lo que va despues del ? en url
+    res.json(personas);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Con un filtro indicando la edad maxima y minima y los amigos que se
+router.get("/", async (req, res) => {
+  try {
+    const ageQuery = Number(req.query.age);
+    const maxAgeQuery = Number(req.query.maxage);
+    const friendQuery = Number(req.query.friends);
+    const friendQ2uery = Number(req.query.friends1);
+    const personas = await coleccion().find(
+      {friends : {$in : [friendQuery, friendQ2uery]},
+      age : {$gt : ageQuery, $lt : maxAgeQuery},
+    }).toArray(); //Query es todo lo que va despues del ? en url
     res.json(personas);
   } catch (err) {
     res.status(404).json(err);
@@ -52,12 +184,68 @@ router.post(`/`, async (req, res) => {
   }
 });
 
+router.post(`/multiple`, async (req, res) => {
+  try {
+      const result = await coleccion().insertMany([req.body.personas]);
+      res.status(201).json(result);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/setAllAges", async (req, res) => {
+  try {
+    const result = await coleccion().updateMany(
+      {},
+      {
+        $set: {
+          age : 33,
+        },
+      }   
+    );
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(404).json(err)
+  }
+});
+
+router.post("/setAllFriends", async (req, res) => {
+  try {
+    const result = await coleccion().updateMany(
+      {},
+      {
+        $set: {
+          friends : ["Paco", "Pepa", "Pepe"]
+        },
+      }   
+    );
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(404).json(err)
+  }
+});
+
 router.put("/:id", async (req, res) => {
   try {
     const result = await coleccion().updateOne(
-      { _id: new ObjectId(req.params?.id) },
-      { $set: req.body }
+      { _id: new ObjectId(req.params?.id) }, //Es un filtro
+      { $set: req.body } //El objeto con la info actualizada
     );
+    res.json(result);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Dejar sin amigos a todos lo que tengan x edad
+router.put("/noFriends/:age", async (req, res) => {
+  try {
+    const ageParam = Number(req.params.age)
+    const result = await coleccion().updateMany({
+      age : {$gte : ageParam},
+    }, {
+      $set : { friends : []},
+    })
     res.json(result);
   } catch (err) {
     res.status(404).json(err);
@@ -69,11 +257,24 @@ router.delete("/:id", async (req, res) => {
     const result = await coleccion().deleteOne({
       _id: new ObjectId(req.params?.id),
     });
+    
     res.json({ result });
   } catch (err) {
     res.status(404).json(err);
   }
 });
 
+router.delete("/ageEqual/:age", async (req, res) => {
+  try {
+    const ageParams= Number(req.params.age);
+    //Antes de esto todas las comprobaciones
+    const result = await coleccion().deleteMany({
+      age : {$eq : ageParams}
+    });
+    res.json({ result });
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
 
 export default router;
